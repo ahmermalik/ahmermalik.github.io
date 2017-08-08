04301b0625866c3cf701239578cc96fada0175b5
ahmermalik <ahmer9800@gmail.com>
2017-08-08 14:35:02 -0500

    Update files

---
+++ admin.js
@@ -0,0 +1,43 @@
+var resultsDiv = document.getElementById('result-div');
+
+
+function analyze() {
+    var creditPrice, crp, cashBack, cab, cashPrice, cap, gallons, ga;
+    creditPrice = document.getElementById("form1");
+    cashBack = document.getElementById("form2");
+    cashPrice= document.getElementById("form3");
+    gallons = document.getElementById("form4");
+    crp=creditPrice.elements["creditPrice"].value;
+    cab=cashBack.elements["cashBack"].value;
+    cap=cashPrice.elements["cashPrice"].value;
+    ga=gallons.elements["gallons"].value;
+
+    var newPrice;
+
+    //calculations
+    cashBack= ((crp)-.01*crp);
+    newPrice = cashBack * ga ;
+    cashPrice = ga * cap;
+
+    //display
+    resultsDiv.innerHTML = "Your per gallon cost with cashback will be: $" + (cashBack)+"<br>";
+    resultsDiv.innerHTML += "To fuel your vehicle using a credit card, it will cost: $" + (newPrice)+"<br>";
+    resultsDiv.innerHTML += "To fuel your vehicle using cash, it will cost: $" + (cashPrice)+"<br>";
+
+
+    if (newPrice > cashPrice) {
+        result = "There will be more savings if you pay using cash.";
+    }
+
+    else {
+        result = "There will be more savings if you pay using credit.";
+    }
+
+    resultsDiv.innerHTML += result;
+
+};
+
+
+
+
+
